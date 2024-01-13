157fd968c94f4e8829920a45d50a2b4a0dfc8f77
Edward A. Roualdes <eroualdes@csuchico.edu>
2024-01-13 11:45:38 -0800

    add more u-turn checks

--- tools.py
+++ tools.py
@@ -1,7 +1,12 @@
 import numpy as np
 
-def u_turn(position, position_new, momentum_new, cut_off = 0.0):
-    return np.dot(position_new - position, momentum_new) <= cut_off
+def u_turn(pb, pf, pt, cut_off = 0.0):
+    normb = np.linalg.norm(pb)
+    normf = np.linalg.norm(pf)
+    normt = np.linalg.norm(pt)
+    f = np.dot(pf, pt) / (normf * normt)
+    b = np.dot(pb, pt) / (normb * normt)
+    return f <= cut_off or b <= cut_off, f, b
 
 def power_two(x):
     return (x != 0) and (x & (x - 1) == 0)
